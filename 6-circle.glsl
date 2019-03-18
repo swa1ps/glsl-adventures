7663eaefad9c053b31430b09271f24d43c9942d9
Alexander Ergin <swaips135@gmail.com>
2019-03-18 23:49:43 +0500

    add circle function

--- 6-circle.glsl
+++ 6-circle.glsl
@@ -4,11 +4,18 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 
+float circle(in vec2 st, in vec2 center, in float radius) {
+    float smooth_border = 0.001;
+    float d = distance(st, center);
+    return smoothstep(radius, radius + smooth_border, d);
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
-    float d = distance(st, vec2(0.5));
-    vec3 color = vec3(smoothstep(0.48, 0.481, d));
+    vec3 color = vec3(
+        circle(st, vec2(0.5), 0.4)
+    );
 
     gl_FragColor = vec4(color, 1.0);
 }
